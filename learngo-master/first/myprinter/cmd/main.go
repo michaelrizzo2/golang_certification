8207f210a88534f55ccfb9d29580092c7a8eb560
michaelrizzo2 <m_rizzo@live.com>
2020-07-12 20:38:28 -0400

    Moving github repository to gopath.

--- learngo-master/first/myprinter/cmd/main.go
+++ learngo-master/first/myprinter/cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import "fmt"
-import "~/golang-certification/learngo-master/first/myprinter/myprinter.go"
+import "golang-certification/learngo-master/first/myprinter/myprinter.go"
 
 func main() {
 	myprinter.Hello()
