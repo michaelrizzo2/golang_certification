dc7d194cb4098e00213b26b2e1e98181fc5a7fe7
michaelrizzo2 <m_rizzo@live.com>
2020-07-14 07:27:36 -0400

    Finished variable declaration.

---
+++ learngo-master/myvariable/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var speed int      // names can start wiath a lowercase or an underscore
+	fmt.Println(speed) //value gets initialized to zero
+}
