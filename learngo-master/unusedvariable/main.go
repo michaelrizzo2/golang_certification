0dc3c1149ff1ecf48e02fd90bc91aba3899a74f1
michaelrizzo2 <m_rizzo@live.com>
2020-07-14 21:13:48 -0400

    Changed the variable from a functino scoes to a package scope.

--- learngo-master/unusedvariable/main.go
+++ learngo-master/unusedvariable/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 )
 
+var speed int
+
 func main() {
 
-	var speed int
 	//go wants evryvariable to be used.do something with it.
 	fmt.Println(speed) //This will do something with the variable
 }
+
+//function scope variables must be used but package scope variables can be declared but not used.
