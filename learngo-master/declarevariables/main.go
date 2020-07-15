fa40c301fafdba73b6256de18d0b0996d0fe9476
michaelrizzo2 <m_rizzo@live.com>
2020-07-14 22:48:15 -0400

    Finshed delcaring varibles of multipel types.

---
+++ learngo-master/declarevariables/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var myage, yourage int
+	var temp float64
+	var success bool
+	var output string
+	fmt.Println(myage, yourage, temp, success, output)
+}
