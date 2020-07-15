6d859d28d5c6494e26745c3c4c6d837e342510d0
michaelrizzo2 <m_rizzo@live.com>
2020-07-14 22:28:39 -0400

    changed multiple variable declaration syntax.

--- learngo-master/zerovariable/main.go
+++ learngo-master/zerovariable/main.go
@@ -5,10 +5,13 @@ import (
 )
 
 func main() {
-	var speed int
-	var heat float64
-	var off bool
-	var brand string
+	//This wil be modified with the new multiple variable syntax.
+	var (
+		speed int
+		heat  float64
+		off   bool
+		brand string
+	)
 	fmt.Println(speed, heat, off, brand)
 	//string variables default to ""
 	// int and floats default ot zero and bolean values default to false.
