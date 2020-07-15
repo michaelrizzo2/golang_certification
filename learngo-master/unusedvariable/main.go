d13b261750a5aec2f61d22dd39b9f6ec38e0042c
michaelrizzo2 <m_rizzo@live.com>
2020-07-14 21:04:34 -0400

    Showed how to get past an unused variable.

---
+++ learngo-master/unusedvariable/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var speed int
+	//go wants evryvariable to be used.do something with it.
+	fmt.Println(speed) //This will do something with the variable
+}
