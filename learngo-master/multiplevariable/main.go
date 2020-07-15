6a5ea80693d8afe8599d78ca137041fbea740198
michaelrizzo2 <m_rizzo@live.com>
2020-07-14 22:38:20 -0400

    Showed how to dclare multiple vriables of the same tyoe.

---
+++ learngo-master/multiplevariable/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	//we can use this declaration to declare multiple variables of the same type.
+	var speed, velocity int
+
+	fmt.Println(speed, velocity)
+}
