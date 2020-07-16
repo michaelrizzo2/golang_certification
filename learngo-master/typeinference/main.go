97c79b05601be3d4d2210247bf8a722a54bb8eff
michaelrizzo2 <m_rizzo@live.com>
2020-07-16 11:24:03 -0400

    1st way of declaring a variable.

---
+++ learngo-master/typeinference/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var safe bool = true
+	fmt.Println(safe)
+
+	//var name type=value is one way
+	//another way is to have go guess the type from the output
+}
