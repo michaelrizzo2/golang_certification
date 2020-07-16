43d881f8f49f402a81962cc6b1ab95088abc944d
michaelrizzo2 <m_rizzo@live.com>
2020-07-16 11:34:19 -0400

    Last way to declare a variable.

--- learngo-master/typeinference/main.go
+++ learngo-master/typeinference/main.go
@@ -10,5 +10,11 @@ func main() {
 
 	//var name type=value is one way
 	//another way is to have go guess the type from the output var name=value
-	//the final way is name:=value
+	//the final way is name:=value this is short declaration.
+
+	var name = "carl"
+	var isscientist = true
+	var age = 62
+	var degree = 5.
+	fmt.Println(name, isscientist, age, degree)
 }
