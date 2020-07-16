af1cb8bfe5af964a250dca91793b497074421223
michaelrizzo2 <m_rizzo@live.com>
2020-07-16 11:28:13 -0400

    Final way to declare a variable.

--- learngo-master/typeinference/main.go
+++ learngo-master/typeinference/main.go
@@ -5,9 +5,10 @@ import (
 )
 
 func main() {
-	var safe = true
+	safe := true
 	fmt.Println(safe)
 
 	//var name type=value is one way
-	//another way is to have go guess the type from the output
+	//another way is to have go guess the type from the output var name=value
+	//the final way is name:=value
 }
