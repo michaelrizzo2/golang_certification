77d092a0e23771567991f5d54edc411ad3cb39cd
michaelrizzo2 <m_rizzo@live.com>
2020-07-17 14:06:09 -0400

    Finished multiple short declaration exercise.

--- learngo-master/06-variables/03-short-declaration/exercises/03-multiple-short-declare-2/main.go
+++ learngo-master/06-variables/03-short-declaration/exercises/03-multiple-short-declare-2/main.go
@@ -8,6 +8,10 @@
 
 package main
 
+import (
+	"fmt"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Multiple Short Declare #2
 //
@@ -22,9 +26,8 @@ package main
 
 func main() {
 	// ADD YOUR DECLARATIONS HERE
-	//
-
+	a, c := 42, "good"
 	// THEN UNCOMMENT THE CODE BELOW
 
-	// fmt.Println(a, c)
+	fmt.Println(a, c)
 }
