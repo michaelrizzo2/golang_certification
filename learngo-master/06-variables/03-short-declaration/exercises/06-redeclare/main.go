03d6dbefcea41a0c751ec3ba7bd824cf8a7d4531
michaelrizzo2 <m_rizzo@live.com>
2020-07-17 14:18:54 -0400

    Finished exercise 6.

--- learngo-master/06-variables/03-short-declaration/exercises/06-redeclare/main.go
+++ learngo-master/06-variables/03-short-declaration/exercises/06-redeclare/main.go
@@ -8,6 +8,10 @@
 
 package main
 
+import (
+	"fmt"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Redeclare
 //
@@ -27,9 +31,9 @@ package main
 
 func main() {
 	// ADD YOUR DECLARATIONS HERE
-	//
-
+	age, yourage := 20, 20
+	age, ratio := 42, 3.14
 	// THEN UNCOMMENT THE CODE BELOW
 
-	// fmt.Println(age, yourAge, ratio)
+	fmt.Println(age, yourage, ratio)
 }
