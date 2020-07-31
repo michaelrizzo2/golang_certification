bc6ecc53820a417397f82bc6196b41ed4a248707
michaelrizzo2 <m_rizzo@live.com>
2020-07-31 16:43:26 -0400

    more basics about type conversion.

--- learngo-master/differenttypes/main.go
+++ learngo-master/differenttypes/main.go
@@ -7,5 +7,12 @@ func main() {
 	var orange int32
 	//int and int32 are not equal
 	apple = int(orange)
-	fmt.Println(apple, orange)
+	_ = apple
+	//This will print ascii characters.
+	orange = 65
+	color := string(orange)
+	fmt.Println(color)
+	//another way to do ascii characters
+	fmt.Println(string([]byte{104, 105}))
+
 }
