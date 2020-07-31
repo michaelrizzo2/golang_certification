e9c6e48f3a6b54afdbe7f38ab6a1f251a7c11c1b
michaelrizzo2 <m_rizzo@live.com>
2020-07-31 16:49:53 -0400

    Exercise 1 finished.

--- learngo-master/06-variables/05-type-conversion/exercises/01-convert-and-fix/main.go
+++ learngo-master/06-variables/05-type-conversion/exercises/01-convert-and-fix/main.go
@@ -8,6 +8,10 @@
 
 package main
 
+import (
+	"fmt"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Convert and Fix
 //
@@ -18,6 +22,6 @@ package main
 // ---------------------------------------------------------
 
 func main() {
-	// a, b := 10, 5.5
-	// fmt.Println(a + b)
+	a, b := 10, 5.5
+	fmt.Println(float64(a) + b)
 }
