f2b9c71d3fb63dbbe5830f3eba0b4d21a281f00e
michaelrizzo2 <m_rizzo@live.com>
2020-09-03 10:45:18 -0400

    Got arguemnt from a user in the command line.

---
+++ learngo-master/command_line/main.go
@@ -0,0 +1,9 @@
+package main
+
+	import ("fmt";"os")
+
+	func main(){
+fmt.Println(os.Args[0])
+}
+//Args[0] is the path of the excutable everythin else after that is the arguements the user specified.
+
