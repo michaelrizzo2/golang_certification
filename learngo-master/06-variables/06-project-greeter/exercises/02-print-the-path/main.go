ae417820119d728104ddfcd5fb5ce0d3dc84f026
michaelrizzo2 <m_rizzo@live.com>
2020-09-04 10:40:49 -0400

    finished path print exercise

--- learngo-master/06-variables/06-project-greeter/exercises/02-print-the-path/main.go
+++ learngo-master/06-variables/06-project-greeter/exercises/02-print-the-path/main.go
@@ -8,6 +8,11 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Print the Path
 //
@@ -23,4 +28,6 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	fmt.Println(os.Args[0])
+
 }
