011f8017faeb5d8f41bc9dda8a0514ee7c6f5a5a
michaelrizzo2 <m_rizzo@live.com>
2020-09-04 10:13:03 -0400

    made simple greeter in o.

---
+++ mygreeter/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	name, age := "michael", 2019
+	fmt.Println("Hello great ", name)
+	fmt.Println("My name is ", name)
+	fmt.Println("My age is ", age)
+}
